cb9768a1a9c5c79beb3ef247f41b36eaa4962e5b
ayuuch <22010@supnum.mr>
2025-03-08 23:34:59 +0000

    la gestion des groupes

--- getionEmploi/app/urls.py
+++ getionEmploi/app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EnseignantViewSet
+from .views import EnseignantViewSet,GroupeViewSet, GroupeMatiereViewSet, ConflitGroupeViewSet
 
 router = DefaultRouter()
 router.register(r'enseignants', EnseignantViewSet, basename='enseignant')
-
+router.register(r'groupes', GroupeViewSet)
+router.register(r'groupe-matieres', GroupeMatiereViewSet)
+router.register(r'conflit-groupes', ConflitGroupeViewSet)
 urlpatterns = [
     path('', include(router.urls)),  
 ]
