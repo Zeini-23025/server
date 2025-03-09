9bf251914c92e917986b76bb6e8fa21bc13bc7ba
AhmedouVallMohamedLemine <ahmedouvallmohamedlemine@gmail.com>
2025-03-09 01:12:44 +0000

    matiere url

--- getionEmploi/app/urls.py
+++ getionEmploi/app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EnseignantViewSet
+from .views import EnseignantViewSet , MatiereViewSet
 
 router = DefaultRouter()
 router.register(r'matieres', MatiereViewSet)
